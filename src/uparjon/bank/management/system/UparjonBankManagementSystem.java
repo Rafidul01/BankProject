380eb469e2e937c33f77017cde872bc958a2a624
Rafidul <rafidulhasan555@gmail.com>
2022-06-22 00:11:33 +0600

    login done

---
+++ src/uparjon/bank/management/system/UparjonBankManagementSystem.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package uparjon.bank.management.system;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+/**
+ *
+ * @author User
+ */
+public class UparjonBankManagementSystem {
+
+    private static final String jdbcURL = "jdbc:mysql://localhost:3306/employeedb";
+    private static final String jdbcUsername = "root";
+    private static final String jdbcPassword = "C211037";
+
+    public static Connection getConnection() {
+        
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver"); 
+            connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
+        } catch (ClassNotFoundException | SQLException e) {
+        }
+        return connection;
+    }
+
+    public static ResultSet queryExecute(String query) throws Exception{
+        Connection connection = getConnection();
+        Statement stmt = connection.createStatement();
+        return stmt.executeQuery(query);
+    }
+    
+    public static int queryUpdate(String query) throws Exception{
+        Connection connection = getConnection();
+        Statement stmt = connection.createStatement();
+        return stmt.executeUpdate(query);
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
